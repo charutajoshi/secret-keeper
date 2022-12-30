e2725e3aaf9088dd7710e696caa22d703182dd23
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2022-12-30 16:57:28 -0600

    Moved session secret to .env file and changed it

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
-  secret: 'secret',
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true }));
 app.use(passport.initialize());
