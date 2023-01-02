d925eb787ef09e91086eabb069eaafb2d8da40ad
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-01 22:48:56 -0600

    Fix body-parser declaration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
